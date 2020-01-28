2ce756d36f76577560845f70b06389af4c792af5
Lucas Polizeli <lumpolizeli@gmail.com>
2020-01-28 20:47:59 -0300

    feat: add routes from UserController

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,9 @@ routes.post('/login', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('/user', handle(UserController.store));
-routes.put('/user/update/:userId', handle(UserController.update));
 routes.get('/user/all', handle(UserController.getAllUsers));
+routes.post('/user/:userId', handle(UserController.getById));
+routes.delete('/user/:userId', handle(UserController.delete));
+routes.put('/user/update/:userId', handle(UserController.update));
 
 export default routes;
