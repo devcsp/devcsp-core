d7e084f229687ce5486813b8a5da4943ad9aaab4
Lucas Polizeli <lumpolizeli@gmail.com>
2020-01-28 09:39:55 -0300

    feat: add database config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import routes from './routes';
 
+import 'dotenv/config';
+import './config/database';
+
 class App {
   constructor() {
     this.server = express();
