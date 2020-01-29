3ef3bd6f9d20365af6e604e1eab0e5615f9ca2d8
Lucas Polizeli <lumpolizeli@gmail.com>
2020-01-28 22:40:57 -0300

    improvement: update job controller

--- src/app/controllers/JobController.js
+++ src/app/controllers/JobController.js
@@ -5,6 +5,21 @@ import jobStatus from '../constants/jobStatus';
 class JobController {
   async store(req, res) {}
 
+  async getJobById(req, res) {
+    const { jobId } = req.params;
+
+    if (!jobId) {
+      return res.status(400).json({ error: 'Missing jobId.' });
+    }
+
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return res.status(400).error({ error: 'Job not found. ' });
+    }
+
+    return res.json(job);
+  }
+
   async getJobByStatus(req, res) {
     const { status } = req.query;
 
