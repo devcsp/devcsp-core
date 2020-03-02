e9eb87d96fe1951d0c07d9b9fc34f9e480c3fd8c
Lucas Polizeli <lumpolizeli@gmail.com>
2020-03-02 08:33:41 -0300

    feat: update routes

--- src/routes.js
+++ src/routes.js
@@ -9,21 +9,21 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/login', handle(SessionController.store));
+routes.post('core/login', handle(SessionController.store));
 
-routes.post('/jobs', handle(JobController.store));
-routes.get('/jobs', handle(JobController.getJobByStatus));
-routes.get('/jobs/:jobId', handle(JobController.getJobById));
-routes.put('/jobs/updateViews/:jobId', handle(JobController.updateJobViews));
+routes.post('core/jobs', handle(JobController.store));
+routes.get('core/jobs', handle(JobController.getJobByStatus));
+routes.get('core/jobs/:jobId', handle(JobController.getJobById));
+routes.put('core/jobs/updateViews/:jobId', handle(JobController.updateJobViews));
 
 // routes.use(authMiddleware);
 
-routes.put('/jobs/:jobId', handle(JobController.updateJob));
+routes.put('core/jobs/:jobId', handle(JobController.updateJob));
 
-routes.post('/user', handle(UserController.store));
-routes.get('/user/all', handle(UserController.getAllUsers));
-routes.post('/user/:userId', handle(UserController.getById));
-routes.delete('/user/:userId', handle(UserController.delete));
-routes.put('/user/update/:userId', handle(UserController.update));
+routes.post('core/user', handle(UserController.store));
+routes.get('core/user/all', handle(UserController.getAllUsers));
+routes.post('core/user/:userId', handle(UserController.getById));
+routes.delete('core/user/:userId', handle(UserController.delete));
+routes.put('core/user/update/:userId', handle(UserController.update));
 
 export default routes;
