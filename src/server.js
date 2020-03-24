6ee7a43bcb551f991753e45220c69bdd80d449f4
Lucas Polizeli <lumpolizeli@gmail.com>
2020-03-23 22:33:00 -0300

    feat: update .env.example

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import app from './app';
 
-app.listen(3000, () => console.log('Rodando na porta 3000'));
+app.listen(process.env.PORT, () => console.log(`Rodando na porta ${process.env.PORT}`));
