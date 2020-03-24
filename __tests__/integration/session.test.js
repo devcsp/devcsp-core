077fdbfb0ba4125fc5d4c58dd81a8ad8b40bdbe4
Lucas Polizeli <lumpolizeli@gmail.com>
2020-03-24 20:57:29 -0300

    feat: implement config to add tests

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+describe('Authentication', () => {
+  it('sum two numbers', () => {
+    const x = 2;
+    const y = 4;
+
+    const sum = x + y;
+
+    expect(sum).toBe(6);
+  });
+});
